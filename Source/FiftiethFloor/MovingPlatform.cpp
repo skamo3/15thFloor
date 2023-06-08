bf355c89ff4c369e0d23cde8e79afadd242cf684
skamo3 <54701846+skamo3@users.noreply.github.com>
2023-06-09 01:24:11 +0900

    리플리 케이트 적용 및 권한에 따른 서버, 클라이언트 로직 분리

--- Source/FiftiethFloor/MovingPlatform.cpp
+++ Source/FiftiethFloor/MovingPlatform.cpp
@@ -3,3 +3,38 @@
 
 #include "MovingPlatform.h"
 
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+	
+
+}
+
+void AMovingPlatform::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+
+	if (HasAuthority())
+	{
+		FVector location = GetActorLocation();
+
+		location += FVector(MoveSpeedX * DeltaSeconds, MoveSpeedY * DeltaSeconds, MoveSpeedZ * DeltaSeconds);
+		SetActorLocation(location);
+	}
+	
+}
--- Source/FiftiethFloor/MovingPlatform.h
+++ Source/FiftiethFloor/MovingPlatform.h
@@ -14,4 +14,21 @@ class FIFTIETHFLOOR_API AMovingPlatform : public AStaticMeshActor
 {
 	GENERATED_BODY()
 	
+private:
+	UPROPERTY(EditAnywhere, Category = "MoveVariable")
+	float MoveSpeedX;
+
+	UPROPERTY(EditAnywhere, Category = "MoveVariable")
+		float MoveSpeedY;
+
+	UPROPERTY(EditAnywhere, Category = "MoveVariable")
+		float MoveSpeedZ;
+
+public:
+	AMovingPlatform();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
 };
