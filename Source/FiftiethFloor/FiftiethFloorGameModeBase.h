ada4702899327e1c13b1aa02f83f3f65a292f0c2
skamo3 <54701846+skamo3@users.noreply.github.com>
2023-06-08 19:01:25 +0900

    First start of the project

---
+++ Source/FiftiethFloor/FiftiethFloorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FiftiethFloorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIFTIETHFLOOR_API AFiftiethFloorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
