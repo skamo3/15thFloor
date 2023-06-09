c1d0c54989c34c26c511975cf41a3e9468b11f85
skamo3 <54701846+skamo3@users.noreply.github.com>
2023-06-10 01:16:40 +0900

    Add Custom gameinstance class & change setting

---
+++ Source/FiftiethFloor/JordiGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "JordiGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIFTIETHFLOOR_API UJordiGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
