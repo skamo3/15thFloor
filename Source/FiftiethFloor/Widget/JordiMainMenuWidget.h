fddfdcb3f0b0355709df5a6796987d4ac4fc1322
skamo3 <54701846+skamo3@users.noreply.github.com>
2023-06-14 19:21:09 +0900

    Change the directory structure

---
+++ Source/FiftiethFloor/Widget/JordiMainMenuWidget.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "JordiMainMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIFTIETHFLOOR_API UJordiMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+
+
+
+protected:
+
+	//virtual void Initialize() override;
+
+
+public:
+	
+};
